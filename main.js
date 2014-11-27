acd9e3a3f0d5aadda2f1d828528ed92d0db79207
Heidi Mason <heidi@musiclaps.com>
2014-11-26 18:28:47 -0800

    First commit

---
+++ main.js
@@ -0,0 +1,27 @@
+var dropdown = $('.dropdown');
+var dropdown_for_windows = $('ul.dropdown.for-windows');
+var dropdown_for_mac = $('ul.dropdown.for-mac');
+
+dropdown.hide();
+
+$('li.for-windows').hover(function() {
+	if ( dropdown_for_windows.is(':hidden') ) {
+		
+		dropdown_for_windows.slideDown('fast');
+
+		// if ($('li.for-windows a span').attr('class') == 'glyphicon-chevron-right') 
+		// {
+		// 	$('li.for-windows a span').attr('class', 'glyphicon-chevron-down');
+		// }
+	} else {
+		dropdown_for_windows.slideUp('fast');
+	}
+});
+
+$('li.for-mac').hover(function() {
+	if ( dropdown_for_mac.is(':hidden') ) {
+		dropdown_for_mac.slideDown('fast');
+	} else {
+		dropdown_for_mac.slideUp('fast');
+	}
+});
