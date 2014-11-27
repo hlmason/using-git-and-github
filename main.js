49b7de0fa37007567f1a8caee1a3d80e48aa627c
Heidi Mason <heidi@musiclaps.com>
2014-11-26 18:36:44 -0800

    Second commit

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-var dropdown = $('.dropdown');
-var dropdown_for_windows = $('ul.dropdown.for-windows');
-var dropdown_for_mac = $('ul.dropdown.for-mac');
+// var dropdown = $('.dropdown');
+// var dropdown_for_windows = $('ul.dropdown.for-windows');
+// var dropdown_for_mac = $('ul.dropdown.for-mac');
 
-dropdown.hide();
+// dropdown.hide();
 
-$('li.for-windows').hover(function() {
-	if ( dropdown_for_windows.is(':hidden') ) {
+// $('li.for-windows').hover(function() {
+// 	if ( dropdown_for_windows.is(':hidden') ) {
 		
-		dropdown_for_windows.slideDown('fast');
+// 		dropdown_for_windows.slideDown('fast');
 
-		// if ($('li.for-windows a span').attr('class') == 'glyphicon-chevron-right') 
-		// {
-		// 	$('li.for-windows a span').attr('class', 'glyphicon-chevron-down');
-		// }
-	} else {
-		dropdown_for_windows.slideUp('fast');
-	}
-});
+// 		// if ($('li.for-windows a span').attr('class') == 'glyphicon-chevron-right') 
+// 		// {
+// 		// 	$('li.for-windows a span').attr('class', 'glyphicon-chevron-down');
+// 		// }
+// 	} else {
+// 		dropdown_for_windows.slideUp('fast');
+// 	}
+// });
 
-$('li.for-mac').hover(function() {
-	if ( dropdown_for_mac.is(':hidden') ) {
-		dropdown_for_mac.slideDown('fast');
-	} else {
-		dropdown_for_mac.slideUp('fast');
-	}
-});
+// $('li.for-mac').hover(function() {
+// 	if ( dropdown_for_mac.is(':hidden') ) {
+// 		dropdown_for_mac.slideDown('fast');
+// 	} else {
+// 		dropdown_for_mac.slideUp('fast');
+// 	}
+// });
